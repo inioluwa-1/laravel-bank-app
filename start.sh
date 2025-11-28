ff230c4dd794ff6f41627e0134f4114295a598a5
ayanbisi inioluwa <ifeini048@gmail.com>
2025-11-28 12:47:37 +0100

    Use migrate:fresh to drop existing tables

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Run migrations
-php artisan migrate --force
+# Fresh migrations (drop all tables and recreate)
+php artisan migrate:fresh --force
 
 # Start the Laravel server
 php artisan serve --host=0.0.0.0 --port=8000
